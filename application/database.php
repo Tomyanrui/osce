3c763d838e111ec911a934473407ee63a6dbcad3
Tomyanrui <1148783218@qq.com>
2017-07-28 10:00:51 +0800

    简介

--- application/database.php
+++ application/database.php
@@ -13,9 +13,9 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '',
+    'hostname'        => '',//什么鬼这是
     // 数据库名
-    'database'        => '',
+    'database'        => '',//git 用不熟啊
     // 用户名
     'username'        => '',
     // 密码
